2a2613464cfb389b152a11a9309dee28b45be529
SergioRodriguez33 <sergioyestevan@gmail.com>
2023-06-12 17:52:53 -0700

    Commiting from terminal

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Pic from '../assets/Myself.JPG';
+import "./About.css"
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+<div>
+    <div className="split left">
+      <div className="centered">
+        <img className="profile_image" src={Pic} alt="Profile Pic"></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Sergio Rodriguez</div>
+        <div className="brief_description">
+          // Details about you goes here
+        </div>
+      </div>
+    </div>
+  </div>
+ 
       </div>
     )
   }
